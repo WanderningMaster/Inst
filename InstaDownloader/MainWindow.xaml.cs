da48271ca2e9d2416a071aef25ab9ad6a54b0184
WanderningMaster <user12341528@gmail.com>
2020-03-05 18:37:33 +0200

    Добавьте файлы проекта.

---
+++ InstaDownloader/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Forms;
+using Microsoft.Win32;
+using System.Drawing;
+using System.Threading;
+
+namespace InstaDownloader
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void FolderExplorerBtn(object sender, RoutedEventArgs e)
+        {
+            FolderBrowserDialog browser = new FolderBrowserDialog();
+            string tempPath = "";
+            
+            //getting save path
+            if (browser.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                tempPath = browser.SelectedPath; 
+            }
+
+            //needful params
+            string rootPath = tempPath;
+            string DownloadUrl = Parser.urlParse(url.Text).Item1;
+            string fileType = Parser.urlParse(url.Text).Item2;
+            
+            //Save path visualize
+            type.Content = rootPath;
+
+            //Downloading file
+            WebClient wc = new WebClient();
+            wc.DownloadFile(DownloadUrl, @"" + rootPath + "/a." + fileType);
+            Status.Content = "Completed";
+
+            //TODO: Add file name menu 
+        }
+    }
+}
---
+++ InstaDownloader/Parser.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InstaDownloader
+{
+    public class Parser
+    {
+        public static Tuple<string, string> urlParse(string url)
+        {
+            string fileType = "";
+
+            //Getting access for json file
+            if (url[url.Length - 1] == '/')
+            {
+                url += "?__a=1";
+            }
+            else
+            {
+                url += "/?__a=1";
+            }
+
+            var json = new WebClient().DownloadString(url);
+            int startIndex;
+
+            //file type definition via json file parsing
+            if(json.Contains("video_url"))
+                startIndex = json.IndexOf("video_url") + 12;
+            else
+                startIndex = json.IndexOf("display_url") + 14;
+
+            //json parsing
+            string output_url = "";
+            while (json[startIndex] != '"')
+            {
+                output_url += json[startIndex];
+                startIndex++;
+            }
+            int index = output_url.IndexOf('?')-1;
+            while(output_url[index] != '.')
+            {
+                fileType += output_url[index];
+                --index;
+            }
+            
+            //briliant way to reverse string
+            char[] charArr = fileType.ToCharArray();
+            Array.Reverse(charArr);
+            fileType = new string(charArr);
+
+            return new Tuple<string, string>(output_url, fileType);
+        }
+    }
+}
